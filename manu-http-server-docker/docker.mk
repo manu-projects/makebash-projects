56eb7f1239d6d3658e9265307fdc95752d9eda48
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-02-10 13:37:53 -0300

    Fix: manu-http-server-docker, preprocesador de css (sass) + refactor makefiles

--- manu-http-server-docker/docker.mk
+++ manu-http-server-docker/docker.mk
@@ -1,15 +1,23 @@
 ##@ Comandos de Docker
+
+# TODO: usar una variable compartida para éste Makefile y en el docker-compose
+# (podría ser una variable de entorno quizás usando export)
+DOCKER_SERVICE=mynodejs
+
+sh: ## Acceder al contenedor con una terminal interactiva
+	@docker-compose exec -it $(DOCKER_SERVICE) /bin/sh
+
 up: ##
-	docker-compose up
+	@docker-compose up
 
 up-rebuild: ##
-	docker-compose up --force-recreate --build
+	@docker-compose up --force-recreate --build
 
 stop: ##
-	docker-compose stop
+	@docker-compose stop
 
 down: ##
-	docker-compose down
+	@docker-compose down
 
 logs: ##
-	docker-compose logs
+	@docker-compose logs
--- manu-http-server-docker/npm.mk
+++ manu-http-server-docker/npm.mk
@@ -1,4 +1,5 @@
-# HTTP_SERVER=$(NPM_BIN_LOCAL_DIRECTORY)/$(PACKAGE_HTTP_SERVER) $(HTTP_SERVER_PARAMS)
+BIN_HTTP_SERVER = $(NPM_BIN_LOCAL_DIRECTORY)/$(PACKAGE_HTTP_SERVER) $(HTTP_SERVER_PARAMS)
+BIN_NPM_SASS = $(NPM_BIN_LOCAL_DIRECTORY)/$(PACKAGE_CSS_PREPROCESSOR) $(NPM_SASS_PARAMS)
 
 ##@ Comandos de NPM
 npm-init: install-npm-packages ## Inicializar proyecto
@@ -11,25 +12,17 @@ endif
 
 # Nota: el doble $ es para que NO lo evalué como una macro de GNU Make
 npm-run: check-npm-bin-directory ## Ejecutar Servidor HTTP
-	$(NPM_BIN_LOCAL_DIRECTORY)/concurrently \
-	"$(NPM_BIN_LOCAL_DIRECTORY)/$(PACKAGE_HTTP_SERVER) $(HTTP_SERVER_PARAMS)" \
-	"$(NPM_BIN_LOCAL_DIRECTORY)/node-sass --watch public/assets/styles.scss public/assets/styles.css"
-
-#	$(NPM_BIN_LOCAL_DIRECTORY)/concurrently \
-#	"npm:$(PACKAGE_HTTP_SERVER) $(HTTP_SERVER_PARAMS)" \
-#	"npm:sass --watch public/assets/styles.scss public/assets/styles.css"
-
-#	$(NPM_BIN_LOCAL_DIRECTORY)/$(PACKAGE_HTTP_SERVER) $(HTTP_SERVER_PARAMS)
+	$(BIN_NPM_SASS) && \
+	$(NPM_BIN_LOCAL_DIRECTORY)/concurrently "$(BIN_HTTP_SERVER)" "$(BIN_NPM_SASS)"
 
 # Nota: en caso de que el otro target run no funcione
 npm-run-alternative: ## Ejecutar Servidor HTTP (Exportando npm como variable de entorno)
 	$(info Agregando la ruta node_modules/.bin como variable de entorno en $$PATH de ésta shell..)
-	@export PATH="$$PATH:$(NPM_BIN_LOCAL_DIRECTORY_ALTERNATIVE)" && \
-	$(PACKAGE_HTTP_SERVER) $(HTTP_SERVER_PARAMS)
+	@export PATH="$$PATH:$(NPM_BIN_LOCAL_DIRECTORY_ALTERNATIVE)" && $(BIN_HTTP_SERVER)
 
 # Nota: con --yes en el npm init, nos evita completar los datos de package.json
 i install-npm-packages: ## Instalar paquetes npm
-	npm init --yes && npm install $(NPM_PACKAGES) --save-dev
+	@npm init --yes && npm install $(NPM_PACKAGES) --save-dev
 
 u update-npm-packages: ## Actualizar paquetes npm
-	npm install $(NPM_PACKAGES) --save-dev
+	@npm install $(NPM_PACKAGES) --save-dev
