6c89a40b03660162b194cb99355fcb6f2dd87d57
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-02-11 12:10:54 -0300

    Añadido: manu-helper, tips comando mv

---
+++ manu-helper/doc/mv.sh
@@ -0,0 +1,5 @@
+# mover todos los archivos y directorios a un directorio, excluyendo el directorio destino
+mv $(ls --ignore=apps/) apps/
+
+# mover todos los archivos a una ruta excluyendo algunos directorios y archivos
+mv $(ls --ignore=.git/ --ignore=.gitignore) nuevo-path/app
