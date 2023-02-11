192dd8f21c2d43b2decb6d44b5f1cb2631f0c37a
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-02-11 19:59:01 -0300

    Añadido: manu-helper, primer comando para docker

---
+++ manu-helper/doc/docker.sh
@@ -0,0 +1,3 @@
+# eliminar todas las imagenes en desuso que son más antiguas a un número de horas
+# (https://docs.docker.com/engine/reference/commandline/image_prune/)
+docker image prune -a --force --filter "until=1h"
