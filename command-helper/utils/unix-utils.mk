38ee3d3786533732b4050e2f7e9dccf72cae4442
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-02-24 16:23:45 -0300

    Añadido: command-helper, macro para comando truncate

--- command-helper/utils/unix-utils.mk
+++ command-helper/utils/unix-utils.mk
@@ -1,2 +1,5 @@
 BAT=bat --line-range $(NUMBER_LINE_BEGIN_DOCUMENTATION):
+
 NAWK_HEADERS=nawk 'BEGIN{print "Comando | Descripción"} {print $$0}' | column -t -s "|"
+
+TRUNCATE_CLEAR_CONTENT=truncate -s 0
