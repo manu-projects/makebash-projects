3eb3055d3d232ff2180fe5345e487726a96457c6
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-03-01 12:59:54 -0300

    Añadido: command-helper, doc sobre comando tr vs sed

--- command-helper/doc/linux-shell/commands/tr.sh
+++ command-helper/doc/linux-shell/commands/tr.sh
@@ -1,12 +1,12 @@
-## CATEGORIA: operaciones con archivos
-## DESCRIPCION: -
+## CATEGORIA: operaciones con la (stdin) y (stdout)
+## DESCRIPCION: a
 
-# transformar todo el texto a mayúsculas
-echo "clojure for the brave and true.pdf" | tr 'a-z' 'A-Z'
-echo "clojure for the brave and true.pdf" | tr '[:lower:]' '[:upper:]'
-echo "clojure for the brave and true.pdf" | sed -E 's/(.+)/\U\1/' # en una regex compleja dificultaría la lectura
+# transformar texto a mayúsculas
+echo "clojure for the brave and true.pdf" | tr 'a-z' 'A-Z' # a-z toma un rango de caracteres
+echo "clojure for the brave and true.pdf" | tr '[:lower:]' '[:upper:]' # utilizamos Clases de Caracteres
+echo "clojure for the brave and true.pdf" | sed -E 's/(.+)/\U\1/' # en una regex compleja haría dificil la lectura
 
-# transformar todo el texto a minúsculas
+# transformar texto a minúsculas
 echo "CLOJURE for the BRAVE and true.pdf" | tr 'A-Z' 'a-z'
 echo "CLOJURE for the BRAVE and true.pdf" | tr '[:upper:]' '[:lower:]'
 echo "CLOJURE for the BRAVE and TRUE.pdf" | sed -E 's/(.+)/\L\1/' # en una regex compleja dificultaría la lectura
@@ -15,9 +15,14 @@ echo "CLOJURE for the BRAVE and TRUE.pdf" | sed -E 's/(.+)/\L\1/' # en una regex
 echo "clojure for the brave and true.pdf" | tr ' ' '-'
 echo "clojure for the brave and true.pdf" | sed 's/ /-/g'
 
-# sustituir las vocales (comando tr Vs comando sed)
-echo "clojure for the brave and true.pdf" | tr 'aeiou' 'x'
-echo "clojure for the brave and true.pdf" | sed 's/[aeiou]/x/g'
+# listar los directorios de la variable de entorno PATH con saltos de linea  (comando tr Vs comando sed)
+echo ${PATH} | tr ':' '\n'
+echo ${PATH} | sed 's/:/\n/g'
+
+# transformar hipervínculo de markdown a orgmode (comando tr Vs comando sed)
+echo "[Guía de ORG Mode](https://orgmode.org/guide)" | tr '()' '[]' | sed -E 's/(.*)/[\1]/' # hará desastres en un archivo markdown
+echo "[Guía de ORG Mode](https://orgmode.org/guide)" | sed -E 's/\((.*)\)/[\1]/; s/(.*)/[\1]/'
+echo "[Guía de ORG Mode](https://orgmode.org/guide)" | sed -E 's/\[(.*)\]\((.*)\)/[[\1][\2]]/' # menos probable de fallar
 
 # sustituir distintos caracteres.. (comando tr Vs comando sed)
 # (Ej. los espacios por guiones, las mayúsculas por minúsculas)
@@ -41,7 +46,7 @@ head -n1 /dev/urandom | tr --complement --delete '[:print:]\n'
 head -n1 /dev/urandom | tr -cd '[:print:]\n'
 
 # sustituir cualquier caracter que no sea alfanumérico (comando tr Vs comando sed)
-# Nota: en una regex ^[expresion] hace de complemento, por tanto sustituimos todo lo que no sea esa expresion
+# Nota: en una regex ^[EXP] hace de Complemento, buscamos todo lo que no coincida con EXP
 echo "clojure for the brave and true.pdf" | tr --complement '[:alnum:]' '-'
 echo "clojure for the brave and true.pdf" | tr -c '[:alnum:]' '-'
 
