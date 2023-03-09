8fb3a41e13f41fd68d27a0a3a6168ec4444c9d2c
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-03-09 02:55:41 -0300

    Añadido: command-helper, opción de edición de comandos y shortcuts

--- command-helper/utils/utils.mk
+++ command-helper/utils/utils.mk
@@ -11,3 +11,10 @@ MENU_CREATE_DOC=whiptail --title "Crear documentación" \
 								--menu "Elegir una opción" 30 80 5 \
 								"linux-create-doc" "Documentación de Linux" \
 								"app-create-doc" "Documentación de una Aplicación"
+
+MENU_EDIT_DOC=whiptail --title "Editar documentación" \
+	--menu "Elegir una opción" 30 80 5 \
+	"linux-edit-commands" "Comandos de Linux" \
+	"linux-edit-shortcuts" "Shortcuts de Linux" \
+	"app-edit-commands" "Comandos de una Aplicación" \
+	"app-edit-shortcuts" "Shortcutsde una Aplicación"
