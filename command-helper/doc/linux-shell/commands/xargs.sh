2a37d4091ec42e2336aaeea55996d83f053206c3
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-03-14 03:39:50 -0300

    Añadido: command-helper, doc comandos cut y uniq + tips de xarg

--- command-helper/doc/linux-shell/commands/xargs.sh
+++ command-helper/doc/linux-shell/commands/xargs.sh
@@ -6,7 +6,9 @@ echo "libros" | xargs --verbose mkdir
 echo "libros" | xargs -t mkdir
 
 # pasar un parámetro a varios comandos
+# - la opción -I de xargs, permite asignarle un símbolo al parámetro redireccionado por el operador pipe a xargs
 echo "libros" | xargs -I % sh -c "mkdir %; touch %"
 
 # buscar archivos por su extensión y moverlos a otra ruta
+# - la opción --verbose de xargs, hará que se imprima (utilizando el comando echo) varias operaciones mv (de cada archivo que encontró el comando find)
 find ./documentos -type f -name "*.txt" | xargs --verbose -I % mv % ./backup
