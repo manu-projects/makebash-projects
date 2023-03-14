6e1edada3948c31a38a889bb7e4b97ebd87b0868
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-03-14 18:48:12 -0300

    Modificado: command-helper, doc comando tee

--- command-helper/doc/linux-shell/commands/tee.sh
+++ command-helper/doc/linux-shell/commands/tee.sh
@@ -1,18 +1,36 @@
 ## CATEGORIA: operaciones con archivos
 ## DESCRIPCION: leer de la (stdin) e imprimir en la (stdout) lo que escribe en archivos
 
-# imprimir por la (stdout) Salida Estandar el resultado que redirecciona de un comando a un archivo
-# (lo agrega sin borrar el contenido actual del archivo)
-ping google.com | tee --append ping.txt
-ping google.com | tee -a ping.txt
+# 1. Escribir en un archivo e imprimir en pantalla (escribir en STDOUT, file descriptor 1), el resultado de un comando que imprime en pantalla (STDOUT)
+#
+# - imprimir en pantalla es lo mismo que escribir en (STDOUT, file descriptor 1)
+# - la opción --append de tee, agrega el contenido al archivo sin borrar el contenido que tuviese
+ping google.com \
+    | tee --append ping.txt
 
-# redireccionar el resultado de un comando, sin imprimir el resultado por pantalla (stdout, standard output)
-ping google.com >> ping.txt
+ping google.com \
+    | tee -a ping.txt
 
-# idem pero asignamos un tiempo límite de 5 segundos,
-# luego envía una señal de interrupción SIGTERM para finalizar la ejecución de los comandos (ping y tee)
-timeout 5s ping google.com | tee -a ping.txt
+# 2. Escribir en un archivo lo que se escribe en pantalla (STDOUT, file descriptor 1)
+#
+# - estamos utilizando el operador de redirección >> de la forma 1>>archivo (por default utiliza el fd 1)
+# - el operador de redirección >> no imprimir por pantalla (STDOUT) lo que escribe en el archivo, pero lo hace el comando tee
+ping google.com \
+     >> ping.txt
 
-# redireccionar un resultado a múltiples archivos
-echo "TODO: personajes en 3d" | tee --append TODO.md TODO.md
-echo "TODO: sprites livianos" | tee -a TODO.md CHANGELOG.md
+ping google.com \
+     1>> ping.txt
+
+# 3. Redireccionar el resultado de un comando a un archivo y a la pantalla (STDOUT),
+# y finalizar su ejecución pasado un tiempo limite
+#
+# - el comando timeout, envía una señal de interrupción SIGTERM para finalizar la ejecución de los comandos (ping y tee)
+timeout 5s ping google.com \
+    | tee -a ping.txt
+
+# 4. Redireccionar un resultado a múltiples archivos
+echo "TODO: personajes en 3d" \
+    | tee --append TODO.md CHANGELOG.md
+
+echo "TODO: sprites livianos" \
+    | tee -a TODO.md CHANGELOG.md
